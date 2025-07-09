446b38a3ab0f4b706c5ed1855ae306bf92cd46b6
Rohit Chauhan <rchauhan0468@conestogac.on.ca>
2025-07-09 11:58:43 -0400

    Changed the mongodo configurations

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,5 @@
 import cors from 'cors';
-import dotenv from 'dotenv';
 import express from 'express';
-import mongoose from 'mongoose';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
